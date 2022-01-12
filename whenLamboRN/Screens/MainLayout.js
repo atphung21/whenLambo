d80b08e19dd2b87c07011336ced1862f5cbf4835
Andy Phung <andytphung@g.ucla.edu>
2022-01-13 00:54:49 +0100

    Main layout screen created

---
+++ whenLamboRN/Screens/MainLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { COLORS, SIZES, icons } from '../Constants/index';
+
+const MainLayout = () => {
+  return (
+    <View>
+      <Text>Main Layout</Text>
+    </View>
+  );
+};
+
+export default MainLayout;
